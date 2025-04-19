45e6f2f426b6dcfb2dfadb38b9254c902bcba823
Ellen McGrory <35548050+EllenMcG@users.noreply.github.com>
2025-04-19 20:02:21 +0000

    Assignment 4 started

---
+++ assignments/assignment04-github.py
@@ -0,0 +1,5 @@
+# Assignment 4 (Github) - Write a program in python (`assignment04-github.py`) 
+# that will read a file from a repository (in this example, `sample_text.txt` was used). 
+# The program should then replace all the instances of the text "Andrew" with your name (Ellen) and 
+# push to Github.  
+
